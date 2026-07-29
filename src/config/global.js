df02f0e34dde03ed083737cc3762dbce513a7075
SENA <cfmartinezs@hotmail.com>
2026-07-29 13:29:23 -0500

    H

--- src/config/global.js
+++ src/config/global.js
@@ -265,7 +265,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/62360025_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
